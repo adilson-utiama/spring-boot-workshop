76b6e685650f6ff67f627c4d46b5b9d3c0d0acf0
Adilson <asu2u@yahoo.com.br>
2016-08-07 18:41:16 -0300

    Exclusao de titulo usando modal do bootstrap

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -66,6 +66,13 @@ public class TituloController {
 		return mv;
 	}
 	
+	@RequestMapping(value = "{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo, RedirectAttributes attributes){
+		titulos.delete(codigo);
+		attributes.addFlashAttribute("mensagem", "Titulo excluido com sucesso!");
+		return "redirect:/titulos";
+	}
+	
 	
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo(){
---
+++ src/main/resources/static/js/cobranca.js
@@ -0,0 +1,23 @@
+$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event){
+	
+	var button = $(event.relatedTarget);
+	
+	var buttonTitulo = button.data('codigo');
+	var tituloDescricao = button.data('descricao');
+	
+	var modal = $(this);
+	var form = modal.find('form');
+	var action = form.data('url-base');
+	if(!action.endsWith('/')){
+		action += '/';
+	}
+	
+	form.attr('action', action + buttonTitulo);
+	modal.find('.modal-body span')
+		.html('Tem certeza que deseja excluir o titulo <strong>' + tituloDescricao + '<strong>?');
+	
+});
+
+$(function(){
+	$('[rel="tooltip"]').tooltip();
+});
