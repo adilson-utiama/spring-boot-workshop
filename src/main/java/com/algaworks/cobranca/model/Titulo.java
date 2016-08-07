ff699fbddfb9460c3d4d22601908f0dd44851abd
Adilson <asu2u@yahoo.com.br>
2016-08-07 15:40:44 -0300

    Bean Validation no cadastro de titulos

--- src/main/java/com/algaworks/cobranca/model/Titulo.java
+++ src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -11,8 +11,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
-import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
@@ -23,12 +27,18 @@ public class Titulo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotEmpty(message = "Descrição deve ser informado.")
+	@Size(max = 60, message = "Descrição não pode conter mais de 60 caracteres")
 	private String descricao;
 	
+	@NotNull(message = "Data de Vencimento deve ser informado.")
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 	
+	@NotNull(message = "Valor deve ser informado.")
+	@DecimalMin(value = "0.01", message = "Valor não pode ser infeior a 0.01")
+	@DecimalMax(value = "9999999.99", message = "Valor não pode ser maior que 9.999.999,99")
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
 	
